0b436e4c2d22499eff1ae1e1a9755631378ae546
joni_no <joni_no@mail.ru>
2021-08-10 14:14:59 +0300

    Return visibility

--- sygnal/gcmpushkin.py
+++ sygnal/gcmpushkin.py
@@ -207,11 +207,11 @@ class GcmPushkin(ConcurrencyLimitedPushkin):
                     collapse_key=data.get("room_id"),
                     notification=messaging.AndroidNotification(
                         tag=data.get("room_id"),
-                        priority="2",
+                        priority="high",
                         default_vibrate_timings=True,
                         sound="default",
                         default_light_settings=True,
-                        visibility="1",
+                        visibility="public",
                     ),
                     priority="normal",
                 ),
